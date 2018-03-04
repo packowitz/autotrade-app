c2f2335b0bdd42400265818871c690b964ef937a
packowitz <PhilippSchum@web.de>
2018-03-05 09:11:39 +1300

    remove leading zero for ten

--- src/providers/services/util.ts
+++ src/providers/services/util.ts
@@ -13,16 +13,16 @@ export class Util {
     let diffInMins: number = Math.floor(diffInSecs / 60);
     if(diffInMins < 60) {
       let secs: number = diffInSecs % 60;
-      return diffInMins + ":" + (secs > 10 ? secs : '0' + secs) + " mins";
+      return diffInMins + ":" + (secs >= 10 ? secs : '0' + secs) + " mins";
     }
     let diffInHours: number = Math.floor(diffInMins / 60);
     if(diffInHours < 48) {
       let mins: number = diffInMins % 60;
-      return diffInHours + ":" + (mins > 10 ? mins : '0' + mins) + " hours";
+      return diffInHours + ":" + (mins >= 10 ? mins : '0' + mins) + " hours";
     }
     let diffInDays: number = Math.floor(diffInHours / 24);
     let hours: number = diffInHours % 24;
-    return diffInDays + ":" + (hours > 10 ? hours : '0' + hours) + " days"
+    return diffInDays + ":" + (hours >= 10 ? hours : '0' + hours) + " days"
   }
 
 }
