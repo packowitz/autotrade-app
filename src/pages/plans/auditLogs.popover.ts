6809b005139f76c73d06853c0adc8acf6f8b2044
packowitz <PhilippSchum@web.de>
2018-03-19 09:42:31 +1300

    small fixes

--- src/pages/plans/auditLogs.popover.ts
+++ src/pages/plans/auditLogs.popover.ts
@@ -12,7 +12,7 @@ import {AuditLog} from "../../providers/domain/auditLog.model";
         </div>
       </ion-list-header>
       <ion-item *ngFor="let log of logs">
-        <div class="flex-vert">
+        <div class="flex-vert" [class.color-failure]="log.level == 'ERROR'">
           <span (click)="log.showMessage = !log.showMessage" class="flex-space-between" [class.pointer]="log.message">{{log.timestamp | date:'HH:mm:ss'}} {{log.title}}<ion-icon *ngIf="log.message" name="arrow-down" md="ios-arrow-down"></ion-icon></span>
           <div *ngIf="log.message && log.showMessage" class="flex-vert">
             <span *ngFor="let msgPart of log.message.split('; ')" class="small-font text-wrap">{{msgPart}}</span>
