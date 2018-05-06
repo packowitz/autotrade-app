7aaa72e3dee056cce4271b09ac3beab975d6210c
packowitz <PhilippSchum@web.de>
2018-05-06 22:26:45 +1200

    remove app specific code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
 import {LoadingPage} from "../pages/loading/loading";
 
 @Component({
@@ -10,12 +8,10 @@ import {LoadingPage} from "../pages/loading/loading";
 export class MyApp {
   rootPage:any = LoadingPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
     });
   }
 }
